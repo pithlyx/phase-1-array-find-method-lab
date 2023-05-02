b3c0c2eb1ad1d39c8013c835f380e3ea2b4afc32
Cody Roberts <pithlyx@gmail.com>
2023-05-02 13:41:19 -0600

    win filter added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function superbowlWin(record){
+    function matchElement(element){
+        return (element.result === "W")
+    }
+    let found = record.find(matchElement)
+    return found !== undefined ? found.year : undefined
+}
+
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     { year: "2012", result: "L"},
+//     { year: "2011", result: "N/A"},
+//     { year: "2010", result: "L"},
+// ]  
+
+// superbowlWin(record)
